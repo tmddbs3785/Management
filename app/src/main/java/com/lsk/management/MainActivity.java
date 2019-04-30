86cb896980883c788dcb961e0bda459cc7392ac4
kimseungyun <tmddbs3785@naver.com>
2019-05-01 00:22:48 +0900

    회원가입및 로그인 UI

--- app/src/main/java/com/lsk/management/MainActivity.java
+++ app/src/main/java/com/lsk/management/MainActivity.java
@@ -3,10 +3,19 @@ package com.lsk.management;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -14,12 +23,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText passwordText = null;
     private Button loginButton = null;
     private TextView registerButton = null;
+    
+    Retrofit retrofit;
+    ApiService apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        retrofit = new Retrofit.Builder().baseUrl(ApiService.API_URL).build();
+        apiService = retrofit.create(ApiService.class);
+
+        Call<ResponseBody> comment = apiService.getComment(1);
+        comment.enqueue(new Callback<ResponseBody>(){
+            @Override
+            public void onResponse(Call<ResponseBody>call, Response<ResponseBody>response){
+                try {
+                    Log.v("Test",response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody>call, Throwable t){
+
+            }
+        });
         idText = (EditText) findViewById(R.id.idText);
         passwordText = (EditText) findViewById(R.id.passwordText);
         loginButton = (Button) findViewById((R.id.loginButton));
